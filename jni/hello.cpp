bf88a8166cc0bcba20e7dabf16d5c273be194a97
arvindkumar0003 <arvindkmr.0003@gmail.com>
2021-11-26 14:16:43 +0530

    added function to calculate mean

--- jni/hello.cpp
+++ jni/hello.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include<iostream.h>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	// Declaration of X and n
+	int X[50];
+	int n;
+	
+	//Entering value of n
+	cout<<"How may elements are there? ";
+	cin>>n;
+	
+	//Entering Values in Array
+	cout<<"Enter elements : ";
+	for(int i=0;i<n;i++){
+		cin>>X[i];
+	}
+	
+	// Finding  ΣX
+	int sum=0;
+	for(int i=0;i<n;i++){
+		sum+=X[i];
+	}
+	
+	//Calculating mean
+	float mean=(float)sum/n;
+	
+	//Priting mean
+	cout<<"Mean of the series is : "<<mean;
+	
+	return 0;
 }
